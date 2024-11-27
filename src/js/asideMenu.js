faa5093b934df461b6637af1e7004529a20a6629
Julia Grigoreva <julia_panina@mail.ru>
2024-11-27 23:25:10 +0300

    add mobile aside menu

--- src/js/asideMenu.js
+++ src/js/asideMenu.js
@@ -1,6 +1,8 @@
 const burger = document.querySelector('.burger');
 const asideMenu = document.querySelector('.aside-menu-wrapper');
+const asideMenuBlock = document.querySelector('.aside-menu');
 const btnCloseAsideMenu = document.querySelector('.btn-close-menu');
+const linkAsideMenu = document.querySelectorAll('.menu-link');
 const bodyEl = document.body;
 
 burger.addEventListener('click', function() {
@@ -10,12 +12,44 @@ burger.addEventListener('click', function() {
 
 asideMenu.addEventListener('click', function (e) {
     if (e.target === e.currentTarget){
+        asideMenuBlock.classList.remove('aside-menu-large');
         asideMenu.classList.remove('active');
         bodyEl.classList.remove('noscroll');
     }
 });
 
-btnCloseAsideMenu.addEventListener('click', function (e) {
-        asideMenu.classList.remove('active');
-        bodyEl.classList.remove('noscroll');
+btnCloseAsideMenu.addEventListener('click', function () {
+    asideMenuBlock.classList.remove('aside-menu-large');
+    asideMenu.classList.remove('active');
+    bodyEl.classList.remove('noscroll');
+    if (window.innerWidth < 1200) {
+        if (asideMenu.querySelector('.open')) {
+            asideMenu.querySelector('.open').classList.remove("open");
+        }
+    }
 });
+
+for (let link of linkAsideMenu) {
+    link.addEventListener('click', function() {
+        let submenu = this.nextElementSibling;
+        if (window.innerWidth < 1200) {
+            if (submenu) {
+                submenu.classList.toggle("open");
+                submenu.closest(".menu-item").classList.toggle('menu-item-bg');
+            }
+        }
+
+        if (window.innerWidth >= 1200) {
+            asideMenuBlock.classList.add('aside-menu-large');
+            if( asideMenuBlock.querySelector('.open')){
+                asideMenuBlock.querySelector('.open').classList.remove('open');
+            }
+            if (submenu) {
+                submenu.classList.add('open');
+            }
+
+        }
+
+    })
+
+}
