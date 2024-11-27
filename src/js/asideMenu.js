787697001cf81c4e99a4b90f290950f95fbd99a8
Julia Grigoreva <julia_panina@mail.ru>
2024-11-27 18:44:18 +0300

    fix table and made aside menu adaptive

--- src/js/asideMenu.js
+++ src/js/asideMenu.js
@@ -1,3 +1,21 @@
 const burger = document.querySelector('.burger');
 const asideMenu = document.querySelector('.aside-menu-wrapper');
+const btnCloseAsideMenu = document.querySelector('.btn-close-menu');
 const bodyEl = document.body;
+
+burger.addEventListener('click', function() {
+    asideMenu.classList.add('active');
+    bodyEl.classList.add('noscroll');
+});
+
+asideMenu.addEventListener('click', function (e) {
+    if (e.target === e.currentTarget){
+        asideMenu.classList.remove('active');
+        bodyEl.classList.remove('noscroll');
+    }
+});
+
+btnCloseAsideMenu.addEventListener('click', function (e) {
+        asideMenu.classList.remove('active');
+        bodyEl.classList.remove('noscroll');
+});
